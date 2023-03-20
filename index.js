40dc6209b4690b0907afecd6bb8d6067f0548efd
beatrice <beatrice.mwenje@student.moringaschool>
2023-03-20 20:42:56 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
-
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(feet){ 
+    if (feet <= 400) {
+      return "This one is on me!"
+    }
+    else if (feet > 2000 && feet < 2500) {
+      return "I will gladly take your thirty bucks."
+    }
+    else {
+      return "No can do."
+    }
+  
+    
+  }
+  feet(199); 
+  feet(2001); 
+  feet(2501); 
+  
+  
+  function ternaryCheckCity(NYC, Pittsburgh){
+  let city = NYC; 
+  return city === 'NYC' ? "Ok, sounds good." : "No go."
+  
+  }
+  
+  function switchOnCharmFromTip(tip){  
+    switch(tip) {
+      case 'generous': 
+            return "Thank you so much.";
+            break;  
+      case 'not as generous': 
+            return "Thank you."; 
+            break; 
+      default: 
+            return "Bye."; 
+    }
+  }
